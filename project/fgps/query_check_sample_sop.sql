cc17ff1bed583f2a17222c9adcfcd330e102c028
thehuy0606 <119041873+thehuy0606@users.noreply.github.com>
2023-12-14 15:53:12 +0700

    Update query_check_sample_sop.sql

--- project/fgps/query_check_sample_sop.sql
+++ project/fgps/query_check_sample_sop.sql
@@ -199,4 +199,55 @@ sop5 as
             )
         where   1=1
             and (lead_next = 1 or lead_prev = 1) 
+    ),
+sop6 as 
+    (
+        select  distinct 
+                order_date
+            ,   order_id 
+            ,   driver_id 
+            ,   period
+            ,   ping_time 
+            ,   accuracy 
+            ,   latitude
+            ,   longitude
+            ,   if(lead_next = 1 or lead_prev = 1, 1, 0) is_conti 
+        from 
+            (
+                select  *
+                    ,   LEAD(latitude , 1) OVER(PARTITION BY order_id ORDER BY ping_time ASC ) next_latitude 
+                    ,   LEAD(longitude, 1) OVER(PARTITION BY order_id ORDER BY ping_time ASC ) next_longitude 
+                    ,   LEAD(latitude, 1) OVER(PARTITION BY order_id ORDER BY ping_time DESC)  prev_latitude 
+                    ,   LEAD(longitude, 1) OVER(PARTITION BY order_id ORDER BY ping_time DESC) prev_longitude 
+                    ,   IF( (latitude  = LEAD(latitude , 1) OVER(PARTITION BY order_id ORDER BY ping_time ASC )) AND 
+                            (longitude = LEAD(longitude, 1) OVER(PARTITION BY order_id ORDER BY ping_time ASC ))
+                            ,1,0
+                        ) AS lead_next 
+                    ,   IF( (latitude  = LEAD(latitude , 1) OVER(PARTITION BY order_id ORDER BY ping_time DESC)) AND 
+                            (longitude = LEAD(longitude, 1) OVER(PARTITION BY order_id ORDER BY ping_time DESC))
+                            ,1,0
+                        ) AS lead_prev 
+                    -- ,   IF(speed_kmh = LEAD(speed_kmh, 1) OVER(PARTITION BY order_id ORDER BY ping_time ASC ),1,0) AS lead_next 
+                    -- ,   IF(speed_kmh = LEAD(speed_kmh, 1) OVER(PARTITION BY order_id ORDER BY ping_time DESC),1,0) AS lead_prev 
+                from 
+                    (
+                        select  order_date
+                            ,   order_id 
+                            ,   driver_id 
+                            ,   period
+                            ,   ping_time 
+                            ,   accuracy 
+                            ,   latitude
+                            ,   longitude
+                            -- ,   if(accuracy <= 5 and period in ('early'), 1, 0) is_sop
+                        from    dev_vnfdbi_opsndrivers.fgps_hit_proxy_sample 
+                        where   1=1
+                            -- and accuracy <= 5
+                            -- and period in ('early')
+                    )
+                -- where   is_sop = 1
+                order by order_date, order_id, ping_time
+            )
+        where   1=1
+            and (lead_next = 1 or lead_prev = 1) 
     )
